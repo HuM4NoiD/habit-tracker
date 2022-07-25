06d97d4c914e2533a4ed2123f1d3d18503638cf4
Jugal Mistry <jugalmistry99@gmail.com>
2022-07-26 01:06:39 +0530

    Setup guard for launching routine with zero tasks

--- app/src/main/java/io/humanoid/habittracker/ui/destinations/routine/detail/RoutineDetailScreen.kt
+++ app/src/main/java/io/humanoid/habittracker/ui/destinations/routine/detail/RoutineDetailScreen.kt
@@ -14,13 +14,18 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.FabPosition
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.SnackbarResult
 import androidx.compose.material.Text
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -36,6 +41,7 @@ import io.humanoid.habittracker.ui.component.TaskListItem
 import io.humanoid.habittracker.ui.destinations.destinations.RoutineInputSheetDestination
 import io.humanoid.habittracker.ui.destinations.destinations.TaskSelectionSheetDestination
 import io.humanoid.habittracker.ui.destinations.destinations.TimerScreenDestination
+import kotlinx.coroutines.launch
 
 @Composable
 @Destination
@@ -82,110 +88,137 @@ private fun RoutineDetailContent(
     }
 
     val tasksState = viewModel.selectedTasksLiveData.observeAsState()
+    val scaffoldState = rememberScaffoldState()
+    val coroutineScope = rememberCoroutineScope()
 
-    Box(
-        modifier = modifier.fillMaxSize()
-    ) {
-        Column(
-            modifier = modifier.fillMaxSize()
-        ) {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(
-                    text = routine.name,
-                    style = MaterialTheme.typography.h4,
-                    modifier = Modifier
-                        .weight(1f)
-                        .padding(start = 32.dp, end = 32.dp, top = 32.dp, bottom = 32.dp)
-                )
-                IconButton(
-                    onClick = {
-                        navigator.navigate(RoutineInputSheetDestination(routine))
-                    },
-                    modifier = Modifier
-                        .padding(32.dp)
-                        .size(48.dp)
-                ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_str_edit),
-                        contentDescription = "Edit this Routine",
-                        modifier = Modifier.size(24.dp)
-                    )
-                }
-            }
-            Card(
-                modifier = Modifier.fillMaxSize(),
-                shape = RoundedCornerShape(topStart = 32.dp, topEnd = 32.dp)
-            ) {
-                Column(modifier = Modifier.fillMaxSize()) {
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(start = 32.dp, end = 32.dp, top = 32.dp)
-                    ) {
-                        Text(
-                            text = "Tasks In this Routine",
-                            modifier = Modifier.weight(1f)
-                        )
-                        IconButton(
-                            onClick = {
-                                navigator.navigate(
+    Scaffold(
+        scaffoldState = scaffoldState,
+        floatingActionButtonPosition = FabPosition.End,
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    if (tasksState.value.isNullOrEmpty()) {
+                        coroutineScope.launch {
+                            val snackbarResult = scaffoldState.snackbarHostState.showSnackbar(
+                                "There are no tasks in this routine!",
+                                "Add Task"
+                            )
+                            when (snackbarResult) {
+                                SnackbarResult.ActionPerformed -> navigator.navigate(
                                     TaskSelectionSheetDestination(
                                         tasksState.value?.map { task -> task.id }?.toLongArray() ?: LongArray(0)
                                     )
                                 )
-                            },
-                            modifier = Modifier
-                                .size(48.dp)
-                        ) {
-                            Icon(
-                                painter = painterResource(id = R.drawable.ic_str_edit),
-                                contentDescription = "Edit this Routine",
-                                modifier = Modifier.size(24.dp)
-                            )
-                        }
-                    }
-                    if (tasksState.value.isNullOrEmpty()) {
-                        Box(
-                            modifier = Modifier.fillMaxSize()
-                        ) {
-                            Text(
-                                text = "There are no tasks for this routine",
-                                textAlign = TextAlign.Center,
-                                style = MaterialTheme.typography.body1,
-                                modifier = Modifier.align(Alignment.Center)
-                            )
+                                else -> {}
+                            }
                         }
                     } else {
-                        LazyColumn(
+                        navigator.navigate(
+                            TimerScreenDestination(
+                                interval = routine.interval,
+                                taskIds = tasksState.value?.map { task -> task.id }?.toLongArray() ?: LongArray(0)
+                            )
+                        )
+                    }
+                },
+                modifier = Modifier
+                    .padding(16.dp)
+            ) {
+                Icon(painter = painterResource(id = R.drawable.ic_str_launch), contentDescription = "Add entry")
+            }
+        }
+    ) {
+        Box(
+            modifier = modifier.fillMaxSize()
+        ) {
+            Column(
+                modifier = modifier.fillMaxSize()
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = routine.name,
+                        style = MaterialTheme.typography.h4,
+                        modifier = Modifier
+                            .weight(1f)
+                            .padding(start = 32.dp, end = 32.dp, top = 32.dp, bottom = 32.dp)
+                    )
+                    IconButton(
+                        onClick = {
+                            navigator.navigate(RoutineInputSheetDestination(routine))
+                        },
+                        modifier = Modifier
+                            .padding(32.dp)
+                            .size(48.dp)
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_str_edit),
+                            contentDescription = "Edit this Routine",
+                            modifier = Modifier.size(24.dp)
+                        )
+                    }
+                }
+                Card(
+                    modifier = Modifier.fillMaxSize(),
+                    shape = RoundedCornerShape(topStart = 32.dp, topEnd = 32.dp)
+                ) {
+                    Column(modifier = Modifier.fillMaxSize()) {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
                             modifier = Modifier
-                                .fillMaxSize(),
-                            verticalArrangement = Arrangement.spacedBy(16.dp),
-                            contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp)
+                                .fillMaxWidth()
+                                .padding(start = 32.dp, end = 32.dp, top = 32.dp)
                         ) {
-                            items(items = tasksState.value!!) { task ->
-                                TaskListItem(task = task)
+                            Text(
+                                text = "Tasks In this Routine",
+                                modifier = Modifier.weight(1f)
+                            )
+                            IconButton(
+                                onClick = {
+                                    navigator.navigate(
+                                        TaskSelectionSheetDestination(
+                                            tasksState.value?.map { task -> task.id }?.toLongArray() ?: LongArray(0)
+                                        )
+                                    )
+                                },
+                                modifier = Modifier
+                                    .size(48.dp)
+                            ) {
+                                Icon(
+                                    painter = painterResource(id = R.drawable.ic_str_edit),
+                                    contentDescription = "Edit this Routine",
+                                    modifier = Modifier.size(24.dp)
+                                )
+                            }
+                        }
+                        if (tasksState.value.isNullOrEmpty()) {
+                            Box(
+                                modifier = Modifier.fillMaxSize()
+                            ) {
+                                Text(
+                                    text = "There are no tasks for this routine",
+                                    textAlign = TextAlign.Center,
+                                    style = MaterialTheme.typography.body1,
+                                    modifier = Modifier.align(Alignment.Center)
+                                )
+                            }
+                        } else {
+                            LazyColumn(
+                                modifier = Modifier
+                                    .fillMaxSize(),
+                                verticalArrangement = Arrangement.spacedBy(16.dp),
+                                contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp)
+                            ) {
+                                items(items = tasksState.value!!) { task ->
+                                    TaskListItem(task = task)
+                                }
                             }
                         }
                     }
                 }
             }
-        }
-        FloatingActionButton(
-            onClick = {
-                navigator.navigate(TimerScreenDestination(
-                    interval = routine.interval,
-                    taskIds = tasksState.value?.map { task -> task.id }?.toLongArray() ?: LongArray(0)
-                ))
-            },
-            modifier = Modifier
-                .align(Alignment.BottomEnd)
-                .padding(16.dp)
-        ) {
-            Icon(painter = painterResource(id = R.drawable.ic_str_launch), contentDescription = "Add entry")
         }
     }
 }
